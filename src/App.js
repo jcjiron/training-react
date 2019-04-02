e46b429e9969b11f267cd316f814fb97ab5b6649
Juan Carlos <jironjuarezjuan@hotmail.com>
2019-04-02 00:10:29 -0600

    feature: added heroes component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './header/Header';
+
+
 import './App.css';
+import Heroes from './heroes/Heroes';
 
 class App extends Component {
 
@@ -15,21 +18,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload
-          </p>
-          <button className="btn btn-primary" onClick={this.daleDonDale}>Hello</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header/>
+        <Heroes/>
       </div>
     );
   }
