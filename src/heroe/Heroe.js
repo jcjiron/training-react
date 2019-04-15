38ac63dc3c5e5f18c01e067347acf1459fc5f83b
Juan Carlos <jironjuarezjuan@hotmail.com>
2019-04-15 01:29:03 -0500

    feature: added image in heroe component

--- src/heroe/Heroe.js
+++ src/heroe/Heroe.js
@@ -19,41 +19,30 @@ class Heroe extends Component {
         this.setState({
             // heroe: this.props.data
             heroe: this.props.data
-        });        
+        });
     }
 
 
     render() {
         return (
-            <div className="card">
-                <h1 className="">{this.state.heroe.nombre} <small>({this.state.heroe.aparicion})</small></h1>
-                <hr />
-                <div className="row">
-                    <div className="col-md-4">
-                        <img src={`../${this.state.heroe.img}`} className="img-fluid" alt="" />
-                        <br /><br /><br />
-                        <a href="" className="btn btn-outline-danger btn-block">Regresar</a>
-                    </div>
-                    <div className="col-md-8">
-                        <h3>{this.state.heroe.nombre}</h3>
-                        <hr />
-                        <p>
-                            {this.state.heroe.bio}
-                         </p>
-                        <div>
-                            <img className="img-logo" src="../assets/img/marvel-logo.png" alt="DC" />
-                            <img src={this.state.heroe.casa === 'DC' ? dc_logo : marvel_logo } alt="Marvel" className="Heroe-logo"/>
-                        </div>
-                    </div>
+            <div className="card p-3 Heroe-card">
+                <img src={this.state.heroe.img} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{this.state.heroe.nombre}</h5>
+                    <p className="card-text Heroe-bio">{this.state.heroe.bio}</p>
+                    <p className="card-text"><small className="text-muted">({this.state.heroe.aparicion})</small></p>
                 </div>
-
+                <div>
+                    <img src={this.state.heroe.casa === 'DC' ? dc_logo : marvel_logo} alt="Marvel" className="Heroe-logo" />
+                </div>
+                <div className="btn btn-primary btn-block"> Ver más</div>
             </div>
         );
     }
 
-    getSnapshotBeforeUpdate(){
+    getSnapshotBeforeUpdate() {
         console.log(this.state);
-        
+
     }
 
 
